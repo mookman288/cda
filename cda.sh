68b9e5d7ab63dd17b4363ed3f241d87669f3360c
Brandon Raitt <administrator@pxoink.net>
2015-02-10 18:22:09 -0500

    Fix syntax error on non-escaped double quote.

--- cda.sh
+++ cda.sh
@@ -163,7 +163,7 @@ KeepAliveTimeout 60
 		<Directory /usr/lib/cgi-bin>
 			 SSLOptions +StdEnvVars
 		</Directory>
-		BrowserMatch \"MSIE [2-6]" \
+		BrowserMatch \"MSIE [2-6]\" \
 			nokeepalive ssl-unclean-shutdown \
 		 	downgrade-1.0 force-response-1.0
 		BrowserMatch \"MSIE [17-9]\" ssl-unclean-shutdown
